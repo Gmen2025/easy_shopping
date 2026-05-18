2513f5f00f4418cae7249e9aabaf4eda71de09bc
Girma Halie <girma.m.halie19@gmail.com>
2026-05-18 17:56:18 -0500

    Update iOS development API URL IP address
    - Change iOS simulator/device API URL from 192.168.1.6 to 192.168.1.7

--- assets/common/baseUrl.js
+++ assets/common/baseUrl.js
@@ -9,7 +9,7 @@ const PRODUCTION_URL = 'https://easy-shop-server-wldr.onrender.com/api/v1/';
 // Development API URLs
 const DEVELOPMENT_URLS = {
   android: 'http://10.0.2.2:3001/api/v1/', // Android emulator
-  ios: 'http://192.168.1.6:3001/api/v1/',     // iOS simulator or real device
+  ios: 'http://192.168.1.7:3001/api/v1/',     // iOS simulator or real device
 };
 
 let baseUrl = '';
